2ed2dd81a0fd6d4741984249720c22ff7a6f0954
moonpxi <paulo.schneider@gmail.com>
2012-03-24 11:06:06 +0000

    Displaying entries in a hacky way

--- features/step_definitions/homepage_steps.rb
+++ features/step_definitions/homepage_steps.rb
@@ -4,7 +4,7 @@ end
 
 Then /^I should see displayed the following entries$/ do |table|
   table.hashes.each_with_index do |entry, i| 
-    entry_node = find("#entry_#{i}")
-    entry.each { |attribute, value| entry_node.find(".#{attribute}").should have_text(value) }
+    entry_node = find(".entry_#{i}")
+    entry.each { |attribute, value| entry_node.find("##{attribute}").should have_content(value) }
   end
 end
