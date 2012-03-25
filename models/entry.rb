72389269346612b442ba13ab036f4df6554683b2
moonpxi <paulo.schneider@gmail.com>
2012-03-25 19:54:01 +0100

    Defining accessor methods for Entry attributes

--- models/entry.rb
+++ models/entry.rb
@@ -1,6 +1,12 @@
 class Entry < Mongomatic::Base
   include Mongomatic::Expectations::Helper
 
+  %w{topic first second source link}.each do |attribute|
+    define_method attribute do
+      self[attribute]
+    end
+  end
+
   def validate
     expectations do
       be_present self['topic'], 'topic cannot be blank'
--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -1,9 +1,13 @@
 require 'spec_helper'
 
 describe "Entry Model" do
-  let(:entry) { Entry.new }
-  
-  it 'can be created' do
-    entry.should_not be_nil
+  it 'provides accessor methods to attributes' do
+    entry = Entry.new('topic' => 't1', 'first' => 'f1', 'second' => 's1', 'source' => 's1', 'link' => 'l1')
+
+    entry.topic.should == 't1'
+    entry.first.should == 'f1'
+    entry.second.should == 's1'
+    entry.source.should == 's1'
+    entry.link.should == 'l1'
   end
 end
