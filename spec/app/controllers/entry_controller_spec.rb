72c37144372b86cca0f480b3ed2922cd2c56d8ff
moonpxi <paulo.schneider@gmail.com>
2012-04-06 19:55:51 +0100

    POST saves new entry

--- spec/app/controllers/entry_controller_spec.rb
+++ spec/app/controllers/entry_controller_spec.rb
@@ -6,16 +6,27 @@ describe 'EntryController' do
     Entry.collection.remove
   end
 
-  it 'should return all existing entries' do
-    entry1 = saved_entry('topic' => 't1', 'first' => 'f1', 'second' => 's1', 'source' => 's1', 'link' => 'l1')
-    entry2 = saved_entry('topic' => 't2', 'first' => 'f2', 'second' => 's2', 'source' => 's2', 'link' => 'l2')
+  describe '(GET list)' do
+    it 'should return all existing entries' do
+      entry1 = saved_entry('topic' => 't1', 'first' => 'f1', 'second' => 's1', 'source' => 's1', 'link' => 'l1')
+      entry2 = saved_entry('topic' => 't2', 'first' => 'f2', 'second' => 's2', 'source' => 's2', 'link' => 'l2')
 
-    get '/'
+      get '/'
 
-    last_response.template.should == '/list'
-    last_response.assigns(:entries).to_a.should == [entry1, entry2]
+      last_response.template.should == '/list'
+      last_response.assigns(:entries).to_a.should == [entry1, entry2]
+    end
   end
 
+  describe '(POST submit)' do
+    it 'should create a new entry and redirect to listing entries' do
+      post '/entry/submit', :entry => { :topic => 'a', :first => 'b', :second => 'c' }
+
+      puts last_response.methods
+      
+      Entry.find.first.topic.should == 'a'
+    end
+  end
 
   def saved_entry(data)
     entry = Entry.new(data)
