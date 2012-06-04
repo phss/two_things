7480106d6b32f3b8282107d0040e04c0965cd197
moonpxi <paulo.schneider@gmail.com>
2012-06-04 21:02:25 +0100

    Submit new entry through a crude popup

--- app/controllers/entry.rb
+++ app/controllers/entry.rb
@@ -9,16 +9,17 @@ TwoThings.controllers :entry do
   get :submit do
     @entry = Entry.new
     
-    render 'submit'
+    render 'submit', :layout => !request.xhr?
   end
 
   post :submit do
+    puts params[:entry]
     @entry = Entry.insert_from(params[:entry])
 
     if @entry.valid?
       redirect url(:entry, :list)
     else
-      render 'submit'
+      render 'submit', :layout => !request.xhr?
     end
   end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,34 @@
-// Put your application scripts here
+$(document).ready(function() {
+  var popup = new Popup('#entryPopup', '#submit_entry', '/entry/submit');
+
+  $('#new_entry').click(function(event) {
+    event.preventDefault();
+    popup.open();
+  });
+});
+
+var Popup = function(popupLocator, submissionLocator, actionUrl) {
+  function popupLoaded() {
+    $(submissionLocator).click(function(event) {
+      event.preventDefault();
+
+      var entry = {};
+      $('#submit_form').find('input[id^="entry_"]').each(function(index, element) {
+        entry[element.id.replace('entry_', '')] = $(element).val();
+      });
+
+      $.post(actionUrl, { entry: entry }, function(data) {
+        $(popupLocator).empty().append(data);
+        popupLoaded();
+      });
+    });
+
+    $(popupLocator).show();
+  }
+
+  return {
+    open: function() {
+      $(popupLocator).load(actionUrl, popupLoaded);
+    }
+  }
+};
